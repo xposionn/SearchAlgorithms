011efb3fd156c39314baa4578f33a04702afc7af
Timor <xposionn@gmail.com>
2020-05-18 00:11:02 +0300

    added with time and with open. exporting solution to output.txt

--- src/Printers/Printer.java
+++ src/Printers/Printer.java
@@ -3,56 +3,75 @@ package Printers;
 import Common.BoardState;
 import Common.Problem;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class Printer {
 
-    public static String printBoard(BoardState boardState){
+    private static final String OUTPUT_FILE_NAME = "output.txt";
+
+
+
+    public static String printBoard(BoardState boardState) {
         int[][] board = boardState.getBoard();
         String output = "";
-        for(int[] row:board){
-            for(int value:row){
-                String valueToPrint = ""+value;
-                if(value==0) {
-                    valueToPrint="_";
+        for (int[] row : board) {
+            for (int value : row) {
+                String valueToPrint = "" + value;
+                if (value == 0) {
+                    valueToPrint = "_";
                 }
-                output+= valueToPrint+(value==row[row.length-1]?"":",");
+                output += valueToPrint + (value == row[row.length - 1] ? "" : ",");
             }
-            output+="\n";
-
+            output += "\n";
         }
-
         return output;
     }
-    public static void printSolution(BoardState boardState) {
+
+
+
+    public static void exportToOutput(Problem p, BoardState finalState, boolean foundSolution, int nodesExpanded, double totalTime) {
+        try {
+            FileWriter write = new FileWriter(OUTPUT_FILE_NAME);
+            PrintWriter print_line = new PrintWriter(write, true);
+            if (foundSolution) {
+                print_line.println(Printer.solutionToString(finalState));
+                print_line.println("Num: " + nodesExpanded);
+                print_line.println("Cost: " + Printer.getPrice(finalState));
+                if(p.withTime()) {
+                    print_line.println(totalTime + " seconds");
+                }
+                print_line.close();
+            } else {
+                print_line.println("no path");
+                print_line.println("Num: " + nodesExpanded);
+                if(p.withTime()) {
+                    print_line.println(totalTime + " seconds");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    //Private Methods
+    private static String solutionToString(BoardState boardState) {
         BoardState current = boardState;
         String solution = "";
         while (current != null) {
-            solution = current.getMovement()+"-" + solution;
+            solution = current.getMovement() + "-" + solution;
             current = current.getParent();
         }
-        System.out.println(solution.substring(1,solution.length()-1));
+        return solution.substring(1, solution.length() - 1);
     }
-
-    public static void printPrice(BoardState boardState){
+    public static int getPrice(BoardState boardState) {
         int cost = 0;
-        while(boardState.getParent()!=null){
-            cost+=boardState.getPaid();
+        while (boardState.getParent() != null) {
+            cost += boardState.getPaid();
             boardState = boardState.getParent();
         }
-        System.out.println(cost);
-    }
-
-    public static void finishSolution(Problem p, BoardState finalState,boolean foundSolution,int nodesExpanded,double totalTime){
-        if(foundSolution){
-            if (!p.isToOpen()) {
-                Printer.printSolution(finalState);
-                System.out.println("Num: "+nodesExpanded);
-                Printer.printPrice(finalState);
-                System.out.println(totalTime+ " seconds");
-            }
-        }else{
-            System.out.println("no path");
-            System.out.println("Num: "+nodesExpanded);
-        }
-
+        return cost;
     }
 }
